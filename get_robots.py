e14c3a8e1bbd16b41573a898e0e94ed7c92bf4b0
Cameron Epstein <cameron.a.epstein@gmail.com>
2016-09-15 11:27:55 +0100

    add imports, initialize maltego lib

--- get_robots.py
+++ get_robots.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python
+
+from MaltegoTransform import *
+import sys
+import os
+import requests
+
+website = sys.argv[1]
+robots = []
+
+m = MaltegoTransform()
+
+try:
+r = requests.get('http://' + website + '/robots.txt')
+if r.status_code == 200:
+robots = str(r.text).split('\n')
+for i in robots:
+m.addEntity('maltego.Phrase', i)
+else:
+m.addUIMessage("No Robots.txt found..")
+except Exception as e:
+m.addUIMessage(str(e))
+
+m.returnOutput()
